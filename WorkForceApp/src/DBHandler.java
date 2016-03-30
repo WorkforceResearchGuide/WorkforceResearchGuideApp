01ee698aa8742061dce4ee38292ccd09ef7cae73
wendy321 <tienyen.lin@gmail.com>
2016-03-30 16:08:44 -0500

    Add funciotns of AddEntity, AddBatchEntity, AddEntityFolderScan

---
+++ WorkForceApp/src/DBHandler.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+
+public class DBHandler {
+	public  boolean addEntity(Entity entity){
+		return true;
+	}
+	
+	public  boolean addEntityBatch(List<Entity> entityList){
+		return true;
+	}
+	
+	public  boolean addEntityFolderScan(List<Entity> entityList){
+		return true;
+	}
+}
---
+++ WorkForceApp/src/Strength.java
@@ -0,0 +1,4 @@
+
+public enum Strength {
+	UNDECIDED, BELIEF, UNBELIEF	
+}
