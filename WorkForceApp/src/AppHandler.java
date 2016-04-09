a21b28b8b240cc4dc977ffcc385db9f160ea9084
ramjuly9 <ramjuly9@gmail.com>
2016-04-09 15:03:25 -0500

    Changes to AppHandler and TemplateProcessor

--- WorkForceApp/src/AppHandler.java
+++ WorkForceApp/src/AppHandler.java
@@ -99,5 +99,4 @@ public class AppHandler {
 		
 		return tprocessor.retrieveAllStrengths();
 	}
-	
 }
--- WorkForceApp/src/TemplateProcessor.java
+++ WorkForceApp/src/TemplateProcessor.java
@@ -75,6 +75,4 @@ public class TemplateProcessor {
 		
 		return db.retrieveAllStrengths();
 	}
-	
-	
 }
